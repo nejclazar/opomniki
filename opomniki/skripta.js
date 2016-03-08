2dd18fcfe15df0b2a13a18ebe81e57c64f6fee45
Nejc Lazar <nejc.lazar96@gmail.com>
2016-03-08 11:39:17 +0000

    Stran pripravljena za produkcijo

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -1,5 +1,26 @@
 window.addEventListener('load', function() {
 	//stran nalozena
+	
+	var izvediPrijavo = function(event){
+		var uporabnik = document.querySelector("#uporabnisko_ime").value;
+		document.querySelector("#uporabnik").innerHTML = uporabnik;
+		document.querySelector(".pokrivalo").style.visibility = "hidden";
+	}
+	
+	//izvedi prijavo
+	document.querySelector("#prijavniGumb").addEventListener("click", izvediPrijavo);
+	
+	//dodaj opomnik
+	var dodajOpomnik = function(event){
+		var naziv_opomnika = document.querySelector("#naziv_opomnika").value;
+		var cas_opomnika = document.querySelector("#cas_opomnika").value;
+		document.querySelector("#naziv_opomnika").value = "";
+		document.querySelector("#cas_opomnika").value = "";
+		
+		document.querySelector("#opomniki").innerHTML += "<div class='opomnik'>  <div class='naziv_opomnika'>" + naziv_opomnika + "</div>  <div class='cas_opomnika'> Opomnik čez <span>" + cas_opomnika + "</span> sekund.</div> </div>";
+	}
+	
+	document.querySelector("#dodajGumb").addEventListener("click", dodajOpomnik);
 		
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
@@ -13,6 +34,15 @@ window.addEventListener('load', function() {
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
+			if(cas == 0){
+				var naziv_opomnika = opomnik.querySelector(".naziv_opomnika").innerHTML;
+				alert("Opomnik! Zadolžitev " + naziv_opomnika + " je potekla.");
+				document.querySelector("#opomniki").removeChild(opomnik);
+			}
+			else{
+				casovnik.innerHTML = cas - 1;
+			}
+			
 		}
 	}
 	setInterval(posodobiOpomnike, 1000);
